a253d82ba70712cc41af5c206096fedbc7c65e12
daeho1234 <daeho9805@naver.com>
2021-03-29 14:05:25 +0900

    third

--- Project76/소스.c
+++ Project76/소스.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
-int main()
+void star(int a)
 {
-	for (int j = 1; j < 6; j++)
+	for (int j = 1; j < a + 1; j++)
 	{
+		for (int i = 0; i < a - j; i++)
+		{
+			printf(" ");
+		}
 		for (int i = 0; i < j; i++)
 		{
 			printf("*");
 		}
 		printf("\n");
 	}
+}
+
+int main()
+{
+	int a;
+	scanf_s("%d", &a);
+	star(a);
 	return 0;
 }
