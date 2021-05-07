f7a6d0b38c4a6aefee25944f7367cc6cbb3e4e7b
daeho1234 <80096249+daeho1234@users.noreply.github.com>
2021-05-07 14:44:49 +0900

    Update test.c

--- Project76/test.c
+++ Project76/test.c
@@ -2,44 +2,25 @@
 #include <stdlib.h>
 #define SWAP(x,y,t) ((t)=(x), (x)=(y), (y)=(t))
 
-typedef struct li
-{
-	int first;
-	int key;
-}li;
-void bubble_sort(li list[], int n)
+void bubble_sort(int list[], int n)
 {
 	int i, j, temp;
 	for (i = n - 1; i > 0; i--)
 	{
 		for (j = 0; j < i; j++)
-			if (list[j].first > list[j + 1].first) {
-				SWAP(list[j].first, list[j + 1].first, temp);
-				SWAP(list[j].key, list[j + 1].key, temp);
+			if (list[j] > list[j + 1]) {
+				SWAP(list[j], list[j + 1], temp);
+				for (int i = 0; i < 5; i++)
+					printf("%d ", list[i]);
+				printf("\n");
 			}
 	}
 }
 
 int main(void)
 {
-	li a[10];
-	int n;
-	scanf_s("%d",&n);
-	for (int i = 0; i < n; i++)
-		scanf_s("%d", &a[i].key);
-	for (int i = 0; i < n; i++) {
-		if (a[i].key == 10)
-		{
-			a[i].first = 1;
-		}
-		else
-		a[i].first = a[i].key;
-	}
+	int a[15] = { 15,10,14,20,31,1,17,11,18,12,6,8,100,23,21 };
 
-	bubble_sort(a, n);
-	for (int k = n-1; k>=0; k--)
-	{
-		printf("%d",a[k].key);
-	}
+	bubble_sort(a, 11);
 	return 0;
 }
